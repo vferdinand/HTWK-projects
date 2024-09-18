9f8d890c938d9462f467aaf91bf688f8569e6f74
vferdinand <149488222+vferdinand@users.noreply.github.com>
2024-09-18 22:24:13 +0200

    Delete 2nd_term/Pruefung_Hnefatafl/aop-pruefung_Hnefatafl directory

--- 2nd_term/Pruefung_Hnefatafl/aop-pruefung_Hnefatafl/src/view/events/ButtonClickedEvent.java
+++
@@ -1,7 +0,0 @@
-package view.events;
-
-public class ButtonClickedEvent extends ViewEvent {
-	public static final ButtonClickedEvent PLACE_ATTACKER = new ButtonClickedEvent();
-	public static final ButtonClickedEvent PLACE_DEFENDER = new ButtonClickedEvent();
-	public static final ButtonClickedEvent REMOVE_PIECE = new ButtonClickedEvent();
-}
